aeb369de55d490b91ef7fab08c5291fda6c88a03
xXLarryTFVWXx <xxlarrytfvwxx@gmail.com>
2022-05-31 14:34:38 -0800

    Code refacoring and removing unneccesary stuff

--- pyhog/bases.py
+++
@@ -1,2 +0,0 @@
-class Word:
-    pass
--- pyhog/entity.py
+++ pyhog/entity.py
@@ -1,12 +1,10 @@
-import ctypes m, math, random, pygame from .graphics import *
+import ctypes, math, random, pygame
+from .graphics import *
+v2 = pygame.Vector2
 atkdur = 0
-
-""" class new_vector(pygame.math.Vector2):
-    def __init__(self, position, sub_pixel=0): """
-
-
+curlvl = None
 class Character(Spritesheet):
-    def __init__(self, pos, surf, characterName, cells:dict={"stand": [0,0,64,64]}):
+    def __init__(self, pos, surf, characterName, cells:dict={"stand": (0,0,64,64)}):
         super().__init__( f"Art/Characters/{characterName}/sheet.png", cells)
         self.surf = surf
         self.gsp = 0
@@ -14,7 +12,7 @@ class Character(Spritesheet):
         self.x = self.y = 20
         self.width = 7*2+1
         self.height = 9*2+1
-        self.vec = pygame.Vector2((self.x, self.y))
+        self.vec = v2((self.x, self.y))
         self.xvel = self.yvel = 0
         self.frc = self.acc = 0.046875
         self.dec = 0.03125
@@ -22,7 +20,16 @@ class Character(Spritesheet):
         self.rect = pygame.Rect(self.vec, (self.width, self.height))
         self.ang = 0
         self.top = 6
-        self.position = new_vector(pos, sub=128)
+        self.position = v2(pos)
+        self._facing = v2(1,0)
+        self.floor_under = False
+
+    @property
+    def facing(self):
+        return self._facing
+    @property.setter
+    def facing(self):
+        self._facing = v2(1,0).rotate(self.ang)
     def update(self, drc: str):
         self.up = self.ang - 90
         if drc > 0:
@@ -31,39 +38,32 @@ class Character(Spritesheet):
                 if self.gsp >= 0:
                     self.gsp = 0.5
             elif self.gsp > 0:
-                self.gsp += acc
+                self.gsp += self.acc
                 if self.gsp > self.top:
                     self.gsp = self.top
         elif drc < 0:
             if self.gsp > 0:
-                self.gsp -= dec
+                self.gsp -= self.dec
                 if self.gsp <= 0:
                     gsp = -0.5
             elif self.gsp < 0:
-                self.gsp -= acc
-                if abs(self.gsp) > top:
-                    self.gsp = -top
+                self.gsp -= self.acc
+                if abs(self.gsp) > self.top:
+                    self.gsp = -self.top
         else:
             self.gsp = min(abs(self.gsp), self.frc) * math.sin(self.gsp)
         self.vec.from_polar((self.gsp, self.ang))
         self.rect = pygame.Rect(*self.vec, self.width, self.height)
-        if self.layer in (0,1):
-            while curlvl["colA"][self.rect.bottom][self.rect.right][3] > 0:
-                self.vec.from_polar((1, self.up))
-                self.rect = pygame.Rect(*self.vec, self.width, self.height)
-            else:
-                while curlvl['colA'][self.rect.bottom][self.rect.left][3] > 0:
-                    self.vec.from_polar((1, self.up))
-                    self.rect = pygame.Rect(*self.vec, self.width, self.height)
-        elif self.layer in (0,2):
-            while curlvl["colB"][self.rect.bottom][self.rect.right][3] > 0:
-                self.vec.from_polar((1, self.up))
-                self.rect = pygame.Rect(*self.vec, self.width, self.height)
-            else:
-                while curlvl['colB'][self.rect.bottom][self.rect.left][3] > 0:
-                    self.vec.from_polar()
-                    self.rect = pygame.Rect(*self.vec, self.width, self.height)
-        # rotate direction vector to that of the current rotation cell of the level.
+        self.floor_under_right_a = curlvl["colA"][self.rect.bottom][self.rect.right][3] > 0
+        self.floor_under_right_b = curlvl["colB"][self.rect.bottom][self.rect.right][3] > 0
+        if self.layer == 0:
+            self.floor_under = self.floor_under_a or self.floor_under_b
+        if self.layer == 1:
+            self.floor_under = self.floor_under_a
+        if self.layer == 2:
+            self.floor_under = self.floor_under_b
+        self.rect = pygame.Rect(self.vec, self.width, self.height)
+        # rotate direction vector to that of the current rotation cell of the level.]
         
 
 class Boss(Character):
@@ -76,10 +76,10 @@ class Boss(Character):
         
     def update(self):
         global atkdur
-        if len(behaviors) >= 2:            
+        if len(self.behaviors) >= 2:            
             if self.atkdur == 0:
                 targetPos = None
-                atk = random.choice(behaviors).lower()
+                atk = random.choice(self.behaviors).lower()
                 if atk['name'] == "moveleft":
                     distance = self.vec.distance_to(pygame.Vector2(-150,self.y))
                 elif atk['name'] == "moveright":

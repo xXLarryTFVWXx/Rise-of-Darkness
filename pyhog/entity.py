6d8f7954d1d2d0e9b347b94aed597cb17fe46408
xXLarryTFVWXx <xxlarrytfvwxx@gmail.com>
2022-06-18 22:33:33 -0800

    little edit

--- pyhog/entity.py
+++ pyhog/entity.py
@@ -7,63 +7,65 @@ class Character(Spritesheet):
     def __init__(self, pos, surf, characterName, cells:dict={"stand": (0,0,64,64)}):
         super().__init__( f"Art/Characters/{characterName}/sheet.png", cells)
         self.surf = surf
-        self.gsp = 0
+        self.ground_speed = 0
         self.up = -90 # this is in degrees
         self.x = self.y = 20
         self.width = 7*2+1
         self.height = 9*2+1
         self.vec = v2((self.x, self.y))
-        self.xvel = self.yvel = 0
-        self.frc = self.acc = 0.046875
-        self.dec = 0.03125
+        self.x_velocity = self.yvelocity = 0
+        self.friction = self.acceleration = 0.046875
+        self.deceleration = 0.03125
         self.layer = 0
         self.rect = pygame.Rect(self.vec, (self.width, self.height))
-        self.ang = 0
+        self.angle = 0
         self.top = 6
         self.position = v2(pos)
         self._facing = v2(1,0)
-        self.floor_under = False
+        self.floor_under_right_a = self.floor_under_right_b = False
+        self.floor_under_left_a = self.floor_under_left_b = False
 
     @property
     def facing(self):
         return self._facing
     @property.setter
-    def facing(self):
-        self._facing = v2(1,0).rotate(self.ang)
+    def facing(self, angle):
+        self._facing = v2(1,0).rotate_ip(angle)
     def update(self, drc: str):
-        self.up = self.ang - 90
+        possible_new_angle_a = curlvl['rotation'][self.rect.bottom << 8 + self.rect.right]
+        possible_new_angle_b = curlvl['rotation'][self.rect.bottom << 8 + self.rect.right]
+        self.up = self.angle - 90
         if drc > 0:
-            if self.gsp < 0:
-                self.gsp += self.dec
-                if self.gsp >= 0:
-                    self.gsp = 0.5
-            elif self.gsp > 0:
-                self.gsp += self.acc
-                if self.gsp > self.top:
-                    self.gsp = self.top
-        elif drc < 0:
-            if self.gsp > 0:
-                self.gsp -= self.dec
-                if self.gsp <= 0:
-                    gsp = -0.5
-            elif self.gsp < 0:
-                self.gsp -= self.acc
-                if abs(self.gsp) > self.top:
-                    self.gsp = -self.top
+            if self.ground_speed < 0:
+                self.ground_speed += self.deceleration
+                if self.ground_speed >= 0:
+                    self.ground_speed = 0.5
+            elif self.ground_speed > 0:
+                self.ground_speed = self.top if self.ground_speed > self.top else self.ground_speed + self.acceleration
+        elif drc == 1:
+            if self.ground_speed > 0:
+                self.ground_speed -= self.deceleration
+                if self.ground_speed <= 0:
+                    self.ground_speed = -0.5
+            elif self.ground_speed < 0:
+                self.ground_speed -= self.acceleration
+                if abs(self.ground_speed) > self.top:
+                    self.ground_speed = -self.top
         else:
-            self.gsp = min(abs(self.gsp), self.frc) * math.sin(self.gsp)
-        self.vec.from_polar((self.gsp, self.ang))
-        self.rect = pygame.Rect(*self.vec, self.width, self.height)
-        self.floor_under_right_a = curlvl["colA"][self.rect.bottom][self.rect.right][3] > 0
-        self.floor_under_right_b = curlvl["colB"][self.rect.bottom][self.rect.right][3] > 0
+            self.ground_speed = min(abs(self.ground_speed), self.friction) * math.sin(self.ground_speed)
+        self.vec += self.facing * self.ground_speed
+        self.rect = pygame.Rect(self.vec, (self.width, self.height))
+        self.floor_under_right_a = curlvl[1][self.rect.bottom][self.rect.right][3] > 0
+        self.floor_under_right_b = curlvl[2][self.rect.bottom][self.rect.right][3] > 0
         if self.layer == 0:
             self.floor_under = self.floor_under_a or self.floor_under_b
         if self.layer == 1:
             self.floor_under = self.floor_under_a
         if self.layer == 2:
             self.floor_under = self.floor_under_b
-        self.rect = pygame.Rect(self.vec, self.width, self.height)
-        # rotate direction vector to that of the current rotation cell of the level.]
+        self.rect = pygame.Rect(self.vec, (self.width, self.height))
+        # rotate direction vector to that of the current rotation cell of the level.
+        self.facing = v2(self.ground_speed,0).rotate(self.angle)
         
 
 class Boss(Character):
@@ -107,4 +109,4 @@ class Boss(Character):
                     self.fire(pygame)
                 if not targetPos == None:
                     atkdur = self.distance_to(pygame.Vector2(*targetPos)/6)
-    # def fire(self, ang):
+    # def fire(self, angle):
